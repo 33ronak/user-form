23ea7c275cb8a4e77b1d2ecfe321af29ed1b53ee
33ronak <08arnab98@gmail.com>
2024-10-24 10:25:56 +0530

    React form practice project initialized

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AddUser = () => {
+
+    const addUserHandler = (event) => {
+        event.preventDefault();    
+    }
+
+    return (
+        <div>
+            <form>
+                <div>
+                    
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text"></input>
+                    </div>
+
+                    <div>
+                        <label htmlFor="age">Age</label>
+                        <input id="age" type="number"></input>
+                    </div>
+
+                    <div>
+                        <button type="submit" onSubmit={addUserHandler}>Add User</button>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    )
+
+}
+
+export default AddUser;
