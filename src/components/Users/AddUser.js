b9ea875d5434dc2ed0dcb67a7613de5d6a6a04e5
33ronak <08arnab98@gmail.com>
2024-10-24 11:06:32 +0530

    Components refactored

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,28 +1,32 @@
 import React from "react";
+import "./AddUser.css"
+import Card from "../UI/Card";
 
 const AddUser = () => {
 
     const addUserHandler = (event) => {
-        event.preventDefault();    
+        event.preventDefault();
     }
 
     return (
         <div>
-            <form>
-                <div>
+            <Card className = "input">
+                <form>
                     <div>
-                        <label htmlFor="username">Username</label>
-                        <input id="username" type="text"></input>
+                        <div>
+                            <label htmlFor="username">Username</label>
+                            <input id="username" type="text"></input>
+                        </div>
+                        <div>
+                            <label htmlFor="age">Age</label>
+                            <input id="age" type="number"></input>
+                        </div>
+                        <div>
+                            <button type="submit" onSubmit={addUserHandler}>Add User</button>
+                        </div>
                     </div>
-                    <div>
-                        <label htmlFor="age">Age</label>
-                        <input id="age" type="number"></input>
-                    </div>
-                    <div>
-                        <button type="submit" onSubmit={addUserHandler}>Add User</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </Card>
         </div>
     )
 
